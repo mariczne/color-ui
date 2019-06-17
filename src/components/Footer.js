a7a67d043dac79b70f434c167b6c71c5a4f57158
Marcin <mariczne@gmail.com>
2019-06-17 11:54:02 +0200

    Dynamic font color in the table to improve readability

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Footer = () => (
-    <footer style={{ display: 'inline-block', position: 'absolute', bottom: '10px', textAlign: 'center' }}>
+    <footer style={{ margin: '20px auto', textAlign: 'center', width: '100%' }}>
       <small>
-        <span>Image hosting provided by <a href="https://imgur.com/">imgur</a>. Images are auto-deleted after 2 minutes</span><br />
+        <span>Image hosting provided by <a href="https://imgur.com/">imgur</a></span><br />
+        <span>Images are deleted automatically after 3 minutes</span><br />
         <span>&copy; 2019 <a href="https://github.com/mariczne">Marcin Kwiek</a></span>
       </small>
     </footer>
--- src/components/Results/Color.js
+++ src/components/Results/Color.js
@@ -3,17 +3,28 @@ import { Table } from 'semantic-ui-react';
 
 const Color = (props) => {
   const { data } = props;
+
+  const cellStyle = { backgroundImage: `linear-gradient(to right, ${data.raw_hex}, ${data.raw_hex})` };
+
+  const spanStyle = { background: 'inherit', WebkitBackgroundClip: 'text', color: 'transparent', filter: 'invert(1) grayscale(1) contrast(999)' };
+
   return (
-    <Table.Row style={{ backgroundColor: data.raw_hex, color: 'white', textShadow: '0 0 3px black' }}>
-      <Table.Cell>
-        {`${Math.round(data.value * 100)}%`}
+    <Table.Row >
+      <Table.Cell style={cellStyle}>
+        <span style={spanStyle}>
+          {`${Math.round(data.value * 100)}%`}
+        </span>
       </Table.Cell>
-      <Table.Cell>
-        {data.w3c.name.replace(/([A-Z])/g, ' $1').trim()}
+      <Table.Cell style={cellStyle}>
+        <span style={spanStyle}>
+          {data.w3c.name.replace(/([A-Z])/g, ' $1').trim()}
+        </span>
       </Table.Cell>
-      <Table.Cell>
-        {data.raw_hex.toUpperCase()}
-      </Table.Cell>
+      <Table.Cell style={cellStyle}>
+        <span style={spanStyle}>
+          {data.raw_hex.toUpperCase()}
+        </span>
+      </Table.Cell >
     </Table.Row>
   );
 };
